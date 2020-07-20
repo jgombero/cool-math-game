007fa503da629df30fed101ba0155a2b42351f3a
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-20 20:56:44 +0000

    Add initialize

--- game.rb
+++ game.rb
@@ -3,4 +3,15 @@ require './question.rb'
 
 class Game 
 
+  def initialize
+    puts "WeLcOmE tO cOoL mAtH gAmEs!!"
+    puts "Please enter your name, Player 1:"
+    name1 = gets.chomp
+    @player1 = Player.new(name1)
+    puts "Please enter your name, Player 2:"
+    name2 = gets.chomp
+    @player2 = Player.new(name2)
+    puts "Ok, #{@player1.name} an #{@player2.name}, let the games being!"
+  end
+
 end
