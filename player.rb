226be96c6adae6438e7480a6d926484ea32bc594
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-20 20:54:24 +0000

    Add initialize and attributes

--- player.rb
+++ player.rb
@@ -1,3 +1,10 @@
 class Player
+  attr_reader :name
+  attr_accessor :life :turn
 
+  def initialize(name = "Loser that didn't enter a name")
+    @name = name
+    @life = 0
+    @turn = false
+  end
 end
