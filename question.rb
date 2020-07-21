6b63f924ac8722e4c8e7c9b02c5132c5ba08cfa5
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-21 00:25:28 +0000

    Add comments

--- question.rb
+++ question.rb
@@ -1,21 +1,25 @@
 class Question
 
+  # Initializes 2 random numbers and a random operator
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
     @operator = ['+', '-'].sample
   end
 
+  # returns question string
   def question
     "What is #{@num1} #{operator_to_string[@operator]} #{@num2}?"
   end
 
+  # returns correct answer to question
   def answer
     operators[@operator].call
   end
 
   private
 
+  # Converts the operator to string
   def operator_to_string
     {
       "+" => 'plus', 
@@ -23,6 +27,7 @@ class Question
     }
   end
 
+  # Hash for operator expressions
   def operators
     {
       "+" => -> {@num1 + @num2},
