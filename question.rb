eac2f5d5c061ec4a3056dda0b7c11ea505b01c65
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-21 00:05:51 +0000

    Implement more methods and move names logic out of initializer

--- question.rb
+++ question.rb
@@ -1,3 +1,33 @@
 class Question
 
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @operator = ['+', '-'].sample
+  end
+
+  def question
+    "What is #{@num1} #{operator_to_string[@operator]} #{@num2}?"
+  end
+
+  def answer
+    operators[@operator].call
+  end
+
+  private
+
+  def operator_to_string
+    {
+      "+" => 'plus', 
+      "-" => 'minus'
+    }
+  end
+
+  def operators
+    {
+      "+" => -> {@num1 + @num2},
+      "-" => -> {@num1 - @num2}
+    }
+  end
+
 end
