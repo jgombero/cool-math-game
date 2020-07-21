a8a2bbeaa40093bd4c8d53cbb28163e2c9fd1872
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-21 00:05:01 +0000

    Add comment

--- player.rb
+++ player.rb
@@ -1,10 +1,10 @@
 class Player
   attr_reader :name
-  attr_accessor :life :turn
+  attr_accessor :life, :turn
 
   def initialize(name = "Anonymous Player")
     @name = name
-    @life = 0
-    @turn = false
+    @life = 3
+    @turn = true  # Turns could be implemented better (array of players, shift and push after each turn?)
   end
 end
